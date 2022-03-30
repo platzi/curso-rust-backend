2ef87d35ccd631296a89bb6bc1493b6cd1b0d9cd
Hector Pulido <hectorandrespp@gmail.com>
2022-03-30 17:33:10 -0500

    editar blogpost

--- blog-platzi/src/main.rs
+++ blog-platzi/src/main.rs
@@ -29,6 +29,9 @@ fn main() {
 
     // let post: Post = diesel::insert_into(posts::table).values(new_post).get_result(&conn).expect("La insertada, falló");
 
+    let post_update = diesel::update(posts.filter(id.eq(3))).set((body.eq("Este es el post que hemos editado otra vez"), title.eq("Mi tercer blogpost"))).get_result::<Post>(&conn).expect("Error en el update");
+
+
     // Select * from posts
     println!("Query sin limites");
     let posts_result = posts.load::<Post>(&conn).expect("Error al ejecutar la query");
@@ -36,41 +39,5 @@ fn main() {
     for post in posts_result {
         println!("{:?}", post);
     }
-
-
-    // Select * from posts limit 1
-    println!("Query con limites");
-    let posts_result = posts.limit(1).load::<Post>(&conn).expect("Error al ejecutar la query");
-
-    for post in posts_result {
-        println!("{:?}", post);
-    }
-
-    // Select * from posts order by id limit 1
-    println!("Query con limites ordenado por id");
-    let posts_result = posts.order(id.desc()).limit(1).load::<Post>(&conn).expect("Error al ejecutar la query");
-
-    for post in posts_result {
-        println!("{:?}", post);
-    }
-
-
-    // Select title, body from posts limit 1
-    println!("Query columnas especificas");
-    let posts_result = posts.select((title, body)).limit(1).load::<PostSimplificado>(&conn).expect("Error al ejecutar la query");
-
-    for post in posts_result {
-        println!("{:?}", post);
-    }
-
-
-    // Select title, body from posts where id = 2 limit 1
-    println!("Query con where");
-    let posts_result = posts.filter(slug.eq("primer-post")).limit(1).load::<Post>(&conn).expect("Error al ejecutar la query");
-
-    for post in posts_result {
-        println!("{:?}", post);
-    }
-
 
 }
