4958a8392458074ff65e59c662636679c646fe16
Hector Pulido <hectorandrespp@gmail.com>
2022-03-30 16:53:01 -0500

    creando registros de blogpost

--- blog-platzi/src/main.rs
+++ blog-platzi/src/main.rs
@@ -17,11 +17,21 @@ fn main() {
 
     let conn = PgConnection::establish(&db_url).expect("No nos pudimos conectar a la base de datos");
 
-    use self::models::Post;
+    use self::models::{Post, NewPost};
+    use self::schema::posts;
     use self::schema::posts::dsl::*;
 
-    // Select * from posts
-    let posts_result = posts.load::<Post>(&conn).expect("Error al ejecutar la query");
+    let new_post = NewPost {
+        title: "Mi segundo blogpost",
+        body: "2 Lorem impsiingsdd",
+        slug: "segundo-post",
+    };
+
+    let post: Post = diesel::insert_into(posts::table).values(new_post).get_result(&conn).expect("La insertada, falló");
+
+
+    // Select * from posts limit 1
+    let posts_result = posts.limit(1).load::<Post>(&conn).expect("Error al ejecutar la query");
 
     for post in posts_result {
         println!("{}", post.title);
--- blog-platzi/src/models.rs
+++ blog-platzi/src/models.rs
@@ -5,3 +5,13 @@ pub struct Post {
     pub slug: String,
     pub body: String,
 }
+
+use super::schema::posts;
+
+#[derive(Insertable)]
+#[table_name="posts"]
+pub struct NewPost<'a> {
+    pub title: &'a str,
+    pub body: &'a str,
+    pub slug: &'a str,
+}
