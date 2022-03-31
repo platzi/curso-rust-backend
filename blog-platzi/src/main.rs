56b14e00c3ab60c56ce097017ac10b45fa72329a
Hector Pulido <hectorandrespp@gmail.com>
2022-03-31 12:17:44 -0500

    tera configurado

--- blog-platzi/src/main.rs
+++ blog-platzi/src/main.rs
@@ -4,6 +4,7 @@ extern crate diesel;
 pub mod schema;
 pub mod models;
 
+use tera::Tera;
 use dotenv::dotenv;
 use std::env;
 
@@ -20,6 +21,17 @@ pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 use self::models::{Post, NewPost, NewPostHandler};
 use self::schema::posts;
 use self::schema::posts::dsl::*;
+
+
+#[get("/prueba-tera")]
+async fn tera_test(template_manager: web::Data<tera::Tera>) -> impl Responder {
+
+    let mut ctx = tera::Context::new();
+
+    HttpResponse::Ok().content_type("text/html").body(
+        template_manager.render("index.html", &ctx).unwrap()
+    )
+}
 
 
 #[get("/")]
@@ -58,10 +70,15 @@ async fn main() -> std::io::Result<()> {
     let pool = Pool::builder().build(connection).expect("No se pudo construir la Pool");
 
     HttpServer::new(move || {
+        
+        let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*")).unwrap();
+        
         App::new()
         .service(index)
         .service(new_post)
+        .service(tera_test)
         .data(pool.clone())
+        .data(tera)
     }).bind(("0.0.0.0", 9900)).unwrap().run().await
 
 }
