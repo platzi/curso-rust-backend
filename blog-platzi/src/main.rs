cdaedc966257d5e39d135385ad66ce5615ea1768
Hector Pulido <hectorandrespp@gmail.com>
2022-03-31 11:58:01 -0500

    Crear blgos

--- blog-platzi/src/main.rs
+++ blog-platzi/src/main.rs
@@ -17,7 +17,7 @@ use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
-use self::models::Post;
+use self::models::{Post, NewPost, NewPostHandler};
 use self::schema::posts;
 use self::schema::posts::dsl::*;
 
@@ -32,7 +32,19 @@ async fn index(pool: web::Data<DbPool>) -> impl Responder {
         },
         Err(err) => HttpResponse::Ok().body("Error al recibir la data")
     }
+}
 
+
+#[post("/new_post")]
+async fn new_post(pool: web::Data<DbPool>, item: web::Json<NewPostHandler>) -> impl Responder {
+    let conn = pool.get().expect("Problemas al traer la base de datos");
+
+    match web::block(move || {Post::create_post(&conn, &item)}).await {
+        Ok(data) => {
+            return HttpResponse::Ok().body(format!("{:?}", data));
+        },
+        Err(err) => HttpResponse::Ok().body("Error al recibir la data")
+    }
 }
 
 
@@ -46,7 +58,10 @@ async fn main() -> std::io::Result<()> {
     let pool = Pool::builder().build(connection).expect("No se pudo construir la Pool");
 
     HttpServer::new(move || {
-        App::new().service(index).data(pool.clone())
+        App::new()
+        .service(index)
+        .service(new_post)
+        .data(pool.clone())
     }).bind(("0.0.0.0", 9900)).unwrap().run().await
 
 }
--- blog-platzi/src/models.rs
+++ blog-platzi/src/models.rs
@@ -1,10 +1,12 @@
-#[derive(Queryable, Debug)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Queryable, Debug, Deserialize, Serialize)]
 pub struct PostSimplificado {
     pub title: String,
     pub body: String,
 }
 
-#[derive(Queryable, Debug)]
+#[derive(Queryable, Debug, Deserialize, Serialize)]
 pub struct Post {
     pub id: i32,
     pub title: String,
@@ -12,6 +14,13 @@ pub struct Post {
     pub body: String,
 }
 
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct NewPostHandler {
+    pub title: String,
+    pub body: String
+}
+
+use diesel::prelude::*;
 use super::schema::posts;
 
 #[derive(Insertable)]
@@ -20,4 +29,29 @@ pub struct NewPost<'a> {
     pub title: &'a str,
     pub body: &'a str,
     pub slug: &'a str,
+}
+
+
+
+impl Post {
+    pub fn slugify(title: &String) -> String {
+        return title.replace(" ", "-").to_lowercase();
+    }
+
+    pub fn create_post<'a> (
+        conn: &PgConnection, 
+        post: &NewPostHandler
+    ) -> Result<Post, diesel::result::Error> {
+
+        let slug = Post::slugify(&post.title.clone());
+
+        let new_post = NewPost{
+            title: &post.title,
+            slug: &slug,
+            body: &post.body
+        };
+
+        diesel::insert_into(posts::table).values(new_post).get_result::<Post>(conn)
+
+    }
 }
